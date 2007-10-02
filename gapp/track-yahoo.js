9f1f2b6b88a2a0eb5043858a149b71595bc1c892
joostdevalk <joostdevalk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-10-02 20:01:50 +0000

    Added tracking for Yahoo's new search assist queries.
    git-svn-id: https://plugins.svn.wordpress.org/google-analytics-for-wordpress/tags/1.2@20733 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ gapp/track-yahoo.js
@@ -0,0 +1,42 @@
+function _uInfo(page) {
+ var p,s="",dm="",pg=_udl.pathname+_udl.search;
+ if (page && page!="") pg=_uES(page,1);
+ if (_ubd.referrer.search(/search.yahoo/) != -1 && _ubd.referrer.search(/&rp=/) != -1) { 
+  var ref = _ubd.referrer;
+  var pkwregex = new RegExp("&rp=([^&]+)");
+  var pkw = pkwregex.exec(ref);
+  var kwregex = new RegExp("&p=([^&]+)");
+  var kw = kwregex.exec(ref);
+  var tldregex = new RegExp("search.yahoo.([^\/]+)");
+  var tldmatch = tldregex.exec(ref);
+  _ur = "http://search.yahoo." + tldmatch[1] + "/search?p=" + kw[1] + "%20[" +pkw[1] +"]";
+ } else {
+  _ur=_ubd.referrer;	
+ }
+ if (!_ur || _ur=="") { _ur="-"; }
+ else {
+  dm=_ubd.domain;
+  if(_utcp && _utcp!="/") dm+=_utcp;
+  p=_ur.indexOf(dm);
+  if ((p>=0) && (p<=8)) { _ur="0"; }
+  if (_ur.indexOf("[")==0 && _ur.lastIndexOf("]")==(_ur.length-1)) { _ur="-"; }
+ }
+ s+="&utmn="+_uu;
+ if (_ufsc) s+=_uBInfo();
+ if (_uctm) s+=_uCInfo();
+ if (_utitle && _ubd.title && _ubd.title!="") s+="&utmdt="+_uES(_ubd.title);
+ if (_udl.hostname && _udl.hostname!="") s+="&utmhn="+_uES(_udl.hostname);
+ s+="&utmr="+_ur;
+ s+="&utmp="+pg;
+ if ((_userv==0 || _userv==2) && _uSP()) {
+  var i=new Image(1,1);
+  i.src=_ugifpath+"?"+"utmwv="+_uwv+s;
+  i.onload=function() {_uVoid();}
+ }
+ if ((_userv==1 || _userv==2) && _uSP()) {
+  var i2=new Image(1,1);
+  i2.src=_ugifpath2+"?"+"utmwv="+_uwv+s+"&utmac="+_uacct+"&utmcc="+_uGCS();
+  i2.onload=function() { _uVoid(); }
+ }
+ return;
+}
