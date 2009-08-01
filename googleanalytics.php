6f36fde8667f2a1e6bf9f5589e634068ec2a36db
joostdevalk <joostdevalk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-08-01 21:29:52 +0000

    Correct version numer in googleanalytics.php
    git-svn-id: https://plugins.svn.wordpress.org/google-analytics-for-wordpress/tags/3.2@141882 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- googleanalytics.php
+++ googleanalytics.php
@@ -4,7 +4,7 @@ Plugin Name: Google Analytics for WordPress
 Plugin URI: http://yoast.com/wordpress/analytics/
 Description: This plugin makes it simple to add Google Analytics with extra search engines and automatic clickout and download tracking to your WordPress blog. 
 Author: Joost de Valk
-Version: 3.1.1
+Version: 3.2
 Requires at least: 2.7
 Author URI: http://yoast.com/
 License: GPL
