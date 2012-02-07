0f22b4cb3585bef2c1da19b69f85029144ef3dc5
joostdevalk <joostdevalk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-02-07 19:23:16 +0000

    Adding GPL license.
    git-svn-id: https://plugins.svn.wordpress.org/google-analytics-for-wordpress/trunk@501807 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- googleanalytics.php
+++ googleanalytics.php
@@ -7,12 +7,28 @@ Author: Joost de Valk
 Version: 4.2.4
 Requires at least: 3.0
 Author URI: http://yoast.com/
-License: GPL
+License: GPL v3
+
+Google Analytics for WordPress
+Copyright (C) 2008-2012, Joost de Valk - joost@yoast.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 // This plugin was originally based on Rich Boakes' Analytics plugin: http://boakes.org/analytics
 
-define('GAWP_VERSION', '4.2.3');
+define('GAWP_VERSION', '4.2.4');
 
 /*
  * Admin User Interface
