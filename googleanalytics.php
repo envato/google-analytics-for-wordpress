33608e1be6ff73584ec165a6906f0d9fdc83b432
joostdevalk <joostdevalk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-31 14:27:42 +0000

    Updated to 4.2.5, see changelog.
    git-svn-id: https://plugins.svn.wordpress.org/google-analytics-for-wordpress/trunk@592924 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- googleanalytics.php
+++ googleanalytics.php
@@ -4,7 +4,7 @@ Plugin Name: Google Analytics for WordPress
 Plugin URI: http://yoast.com/wordpress/google-analytics/#utm_source=wordpress&utm_medium=plugin&utm_campaign=wpgaplugin&utm_content=v420
 Description: This plugin makes it simple to add Google Analytics to your WordPress blog, adding lots of features, eg. custom variables and automatic clickout and download tracking. 
 Author: Joost de Valk
-Version: 4.2.4
+Version: 4.2.5
 Requires at least: 3.0
 Author URI: http://yoast.com/
 License: GPL v3
@@ -28,7 +28,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // This plugin was originally based on Rich Boakes' Analytics plugin: http://boakes.org/analytics
 
-define( 'GAWP_VERSION', '4.2.4' );
+define( 'GAWP_VERSION', '4.2.5' );
 
 /*
  * Admin User Interface
